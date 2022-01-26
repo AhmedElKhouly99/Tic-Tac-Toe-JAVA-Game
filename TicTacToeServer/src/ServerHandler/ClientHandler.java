f10c93f02fb4ec3df725a5fd8da70fbc584f2eb8
Abanoub Kamal <abanoub.kamal1998@gmail.com>
2022-01-27 01:24:48 +0200

    solve mini bug in ServerHandler package

--- TicTacToeServer/src/ServerHandler/ClientHandler.java
+++ TicTacToeServer/src/ServerHandler/ClientHandler.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tic.tac.toe_server;
+package ServerHandler;
 
 import java.io.DataInputStream;
 import java.io.IOException;
